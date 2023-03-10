c5688ca3a5f823c8a2b6365783d98bc06a1c7c92
Ryan Weispfenning <weisp014@gmail.com>
2023-03-10 16:36:34 -0600

    updated .html file and getting data from database on page load

---
+++ server/public/scripts/client.js
@@ -0,0 +1,43 @@
+$(document).ready(onReady());
+
+function onReady() {
+    console.log('document loaded');
+    //TODO setup click listeners
+
+    //load tasks from database on page load
+    getTasks();
+}
+
+// GET list of tasks from database
+function getTasks() {
+    console.log('inside getTasks');
+    $.ajax({
+        type: 'GET',
+        url: '/list'
+    }).then((response) => {
+        console.log(response);
+        //render to page
+        renderTasks(response);
+    }).catch((error) => {
+        console.log('error in GET:', error);
+    });
+}
+
+function renderTasks(list) {
+    console.log('inside renderTasks');
+    //clear table
+    $('#taskList').empty();
+
+    for (let task of list) {
+        //TODO check if complete and change how its shown
+        $('#taskList').append(`
+        <tr data-id=${task.id}>
+        <td>
+            <input type="checkbox" data-id="${task.id}" data-complete="${task.complete}">
+        </td>
+        <td>${task.description}</td>
+        <td><button id="deleteBtn">Delete</button></td>
+        </td>
+        `);
+    }
+}
