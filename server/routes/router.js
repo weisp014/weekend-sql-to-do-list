f61e2d81046dc9ef61810681d5995614498f7b4c
Ryan Weispfenning <weisp014@gmail.com>
2023-03-13 20:54:28 -0500

    fixed syntax error

--- server/routes/router.js
+++ server/routes/router.js
@@ -13,10 +13,11 @@ if (process.env.DATABASE_URL) {
     });
 } else {
     pool = new pg.Pool({
-    database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: 5432
-});
+        database: 'weekend-to-do-app',
+        host: 'localhost',
+        port: 5432
+    });
+}
 
 // GET
 router.get('/', (req, res) => {
@@ -24,10 +25,10 @@ router.get('/', (req, res) => {
     pool.query(sqlText).then(result => {
         res.send(result.rows);
     })
-    .catch(error => {
-        console.log('error getting tasks', error);
-        res.sendStatus(500);
-    });
+        .catch(error => {
+            console.log('error getting tasks', error);
+            res.sendStatus(500);
+        });
 });
 
 // PUT
@@ -37,13 +38,13 @@ router.put('/:id', (req, res) => {
     const idToUpdate = req.params.id;
     const sqlText = `UPDATE "tasks" SET "complete"=$1 WHERE "id"=$2;`;
     pool.query(sqlText, [status, idToUpdate])
-    .then((result) => {
-        console.log('update successful for task id:', idToUpdate);
-        res.sendStatus(200);
-    })
-    .catch((error) => {
-        console.log(`Error making database query: ${sqlText}`, error);
-    })
+        .then((result) => {
+            console.log('update successful for task id:', idToUpdate);
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log(`Error making database query: ${sqlText}`, error);
+        })
 })
 
 // POST
@@ -52,12 +53,12 @@ router.post('/', (req, res) => {
     console.log('Adding task:', newTask);
     const sqlText = `INSERT INTO "tasks" ("description") VALUES ($1);`;
     pool.query(sqlText, [newTask])
-    .then((result) => {
-        res.sendStatus(201);
-    })
-    .catch((error) => {
-        console.log(`Error making database query: ${sqlText}`, error);
-    });
+        .then((result) => {
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database query: ${sqlText}`, error);
+        });
 });
 
 // DELETE
@@ -66,13 +67,13 @@ router.delete('/:id', (req, res) => {
     const idToDelete = req.params.id;
     const sqlText = `DELETE FROM "tasks" WHERE "id"=$1;`;
     pool.query(sqlText, [idToDelete])
-    .then((result) => {
-        console.log('Delete successful');
-        res.sendStatus(200);
-    })
-    .catch((error) => {
-        console.log(`Error making database query: ${sqlText}`, error);
-    });
+        .then((result) => {
+            console.log('Delete successful');
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log(`Error making database query: ${sqlText}`, error);
+        });
 });
 
 module.exports = router;
